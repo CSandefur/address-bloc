a2c8b03e07cd49b0ac22364961c89a4329817293
Chelcy Sandefur <chelcy.sandefur@gmail.com>
2019-11-21 15:31:35 -0800

    Assignment Creating Contacts and Connecting to a Database Completed

--- spec/contact_controller_spec.js
+++ spec/contact_controller_spec.js
@@ -23,9 +23,10 @@ describe("ContactController", () => {
 
     it("should add a single contact into the book", (done) => {
 
-      this.book.addContact("Alice", "001-101-1010").then((contact) => {
+      this.book.addContact("Alice", "001-101-1010", "alice@gmail.com").then((contact) => {
         expect(contact.name).toBe("Alice");
         expect(contact.phone).toBe("001-101-1010");
+        expect(contact.email).toBe("alice@gmail.com");
         done();
       })
       .catch((err) => {
